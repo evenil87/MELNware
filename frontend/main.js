4b9d249e7721fd1ebbcbd9b75878e46c60c90a45
Lompish <linda.hiermann.data24sto@edu.tucsweden.se>
2025-09-03 15:57:55 +0200

    Added frontend files

--- frontend/main.js
+++ frontend/main.js
@@ -1,34 +1,29 @@
-// Grab the input field firstName
-let inputField = document.querySelector('input[name="firstName"]');
-// Listen to when the user types a character in the field
-inputField.addEventListener('keyup', async () => {
-  // Read the value of the input field
-  let searchValue = inputField.value;
-  // Grab the main element
-  let main = document.querySelector('main');
-  // If the value is empty do not try to search
-  // but empty the main element
-  if (searchValue === '') {
-    main.innerHTML = '';
-    return;
-  }
-  // Ask the database to search for users via a REST-api route
-  let rawData = await fetch('/api/search-by-firstname/' + searchValue);
-  // Convert rawData from json to a js data structure
-  let data = await rawData.json();
+import { startPageContent } from './start-page.js';
+import { musicSearchPageContent } from './music-search.js';
 
-  // Convert the data to html
-  let html = '';
-  for (let { id, firstName, lastName, email } of data) {
-    html += `
-      <article>
-        <h2>${firstName} ${lastName}</h2>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Id:</b> ${id}</p>
-      </article>
-    `;
-  }
-  // Replace the content of the main element with
-  // our new html (the data converted from json)
-  main.innerHTML = html;
+// Click on menu link
+document.body.addEventListener('click', event => {
+  let navLink = event.target.closest('header nav a');
+  if (!navLink) { return; }
+  // don't try to follow the link in the a tag
+  event.preventDefault();
+  // read the text in the link
+  let linkText = navLink.textContent;
+  // show correct content depending on menu choice
+  showContent(linkText);
 });
+
+// Function to show page content
+function showContent(label) {
+  let content;
+  if (label === 'Start') {
+    content = startPageContent();
+  }
+  else if (label === 'Sök musik') {
+    content = musicSearchPageContent();
+  }
+  document.querySelector('main').innerHTML = content;
+}
+
+// When the page loads
+showContent('Start');
---
+++ frontend/startPage.js
@@ -0,0 +1,9 @@
+export function startPageContent() {
+  return `
+    <h1>Start</h1>
+    <p>Välkommen till MELNware, ett enkelt verktyg för att söka i din musik- och ljudsamling.</p>
+    <p>Du kan söka på artist, album, låttitel, genre och årtal.</p>
+    <p>Du kan också söka på delar av ord, t.ex. "love" för att hitta "Love Me Do" och "All You Need Is Love".</p>
+    <p>Du kan kombinera sökord med mellanslag för att hitta lå  tar som matchar alla ord, t.ex. "Beatles love".</p>
+  `;
+}
