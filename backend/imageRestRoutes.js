3da99f2b2729488a050427c8d987509fc3a45183
Malin Hööglund <191246377+MalinHoog@users.noreply.github.com>
2025-09-15 11:50:50 +0200

    trying to make geomap work, it is not at the moment, but saving just because

--- backend/imageRestRoutes.js
+++ backend/imageRestRoutes.js
@@ -41,4 +41,18 @@ export default function setupImageRestRoutes(app, db) {
     res.json(result[0] || {});
   });
 
+  app.get('/api/images', async (req, res) => {
+    let data = await fetch('/api/images');
+    let images = await data.json();
+    let html = '';
+    for (let image of images) {
+      let { latitude, longitude } = image.metadata;
+      html += `<section>
+      <a href="https://maps.google.com/?q=${image.metadata.latitude},${image.metadata.longitude}" target="_blank">
+        <img src="/images/${image.fileName}" alt="Arable land at latitude ${latitude}, longitude ${longitude}">
+      </a>
+    </section>`;
+    }
+    document.querySelector('article').innerHTML = html;
+  });
 }
